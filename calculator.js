0a41b71a83a863be016bc9a510eec0e1ba3fa5c5
sahnoune2 <anankarta@gmail.com>
2023-06-19 17:15:53 +0100

    calculator.js

--- calculator.js
+++ calculator.js
@@ -1,9 +1,31 @@
-function calc(x, y) {
-  if (typeof x === "number" && typeof y === "number") {
-    return x + y;
-  } else {
-    return "error ,check ur entries";
-  }
-}
-
-export { calc };
+const myObject = {
+  add: function (x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+      return x + y;
+    } else {
+      return "error ,check ur entries";
+    }
+  },
+  sub: function (x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+      return x - y;
+    } else {
+      return "error ,check ur entries";
+    }
+  },
+  mult: function (x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+      return x * y;
+    } else {
+      return "error ,check ur entries";
+    }
+  },
+  div: function (x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+      return x / y;
+    } else {
+      return "error ,check ur entries";
+    }
+  },
+};
+export { myObject };
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,9 +1,21 @@
-import { calc } from "../calculator";
+import { myObject } from "../calculator";
 
-test("calc", () => {
-  expect(calc(1, 2)).toBe(3);
+test("add", () => {
+  expect(myObject.add(1, 2)).toBe(3);
 });
 
-test("calc", () => {
-  expect(calc(1, "a")).toBe("error ,check ur entries");
+test("add", () => {
+  expect(myObject.add(1, "a")).toBe("error ,check ur entries");
+});
+
+test("sub", () => {
+  expect(myObject.sub(1, 2)).toBe(-1);
+});
+
+test("mult", () => {
+  expect(myObject.mult(1, 2)).toBe(2);
+});
+
+test("div", () => {
+  expect(myObject.div(1, 0)).toBe(Infinity);
 });
